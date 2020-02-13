53d131d3e19fdfa4fd57a56f0085c44500aa20b7
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-13 18:34:18 +0900

    define new and create method
    newメソッドとcreateメソッドの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,22 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
