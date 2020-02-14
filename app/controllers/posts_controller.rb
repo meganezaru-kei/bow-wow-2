50715ed8ae65e30939cb4a971d48515878ae7f5e
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-14 19:12:17 +0900

    Implement pagination function
    ページネーション機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_target_post, only: %i[show edit update destroy]
   
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env == 'development'
+  (1..50).each do |i|
+    Post.create(title: "タイトル#{i}", body: "本文#{i}")
+  end
+end
