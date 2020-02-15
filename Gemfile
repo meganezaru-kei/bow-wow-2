925d84b03faeaacd3e6e2a0143ca6a4dbba93654
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-15 14:41:23 +0900

    Implemented comment saving function
    コメント保存処理機能の実装

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano3-unicorn'
+  gem 'rails-flog', require: 'flog'
 end
 
 group :development do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.post
+    else
+      redirect_to post_path(comment.post.id), flash: {
+        comment: comment,
+        error_messages: comment.errors.full_messages
+      }
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :comment)
   end
 end
