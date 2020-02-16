8766ebeefe49b986fa770318bf5359db41662c68
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-16 21:36:10 +0900

    Create devise_helper.rb
    deviseヘルパーメソッドの作成

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+module DeviseHelper
+  def bootstrap_alert(key)
+    case key
+    when "alert"
+      "danger"
+    when "notice"
+      "primary"
+    when "error"
+      "warning"
+    end
+  end
+
+  def switch_flash(existing_key, new_key)
+    message = flash[existing_key].dup
+    flash.discard existing_key
+    flash[new_key] = message
+  end
+end
