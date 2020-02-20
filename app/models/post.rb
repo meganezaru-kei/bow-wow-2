c666f65485422303b8e097edc96ee32a04a783d0
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-20 18:17:18 +0900

    implement image post function
    画像投稿機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,9 +14,11 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :post_tag_relations, dependent: :delete_all
   has_many :tags, through: :post_tag_relations 
-  has_many_attached :images
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 1000 }
+
+  has_many_attached :images
+  validates :images, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
 end
