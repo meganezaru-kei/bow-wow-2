adb4762cfaada48b52f3b5668dedd8072a2dd7f3
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-22 19:01:46 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, 'config/master.key'
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
