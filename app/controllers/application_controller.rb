67d9ec54241afb6ca36cf2db64c0783058b4c878
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-24 16:16:14 +0900

    Update search form
    検索フォームの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_ransack
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_ransack
+    @q = Post.ransack(params[:q])
   end
 end
