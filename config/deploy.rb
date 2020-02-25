64225369725639008fade53c336f7e001733abf1
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-25 19:11:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # master.key用のシンボリックリンクを追加
-# set :linked_files, fetch(:linked_files, []).push('config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
