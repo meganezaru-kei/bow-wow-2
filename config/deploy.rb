9ff67bfa647956bf671e70dd6307566c28db2f2d
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-25 14:57:21 +0900

    test auto deploy
    自動デプロイのテスト

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,15 +9,14 @@ set :application, 'bow-wow-2'
 set :repo_url,  'git@github.com:meganezaru-kei/bow-wow-2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_files, fetch(:linked_files, []).push('config/master.key')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' 
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/BowWow.pem'] 
+keys: ['~/.ssh/BowWow.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -26,10 +25,23 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# master.key用のシンボリックリンクを追加
+# set :linked_files, fetch(:linked_files, []).push('config/master.key')
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+  
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |_host|
+      execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
+      # upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
