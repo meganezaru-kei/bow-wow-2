cbb2be1c28674c8596ba6146ccf02bcc0b966c73
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-25 23:03:24 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -65,6 +65,7 @@ server '18.176.39.233',
   roles: %w{app db web},
   port: 22,
   ssh_options: {
+    user: 'ec2-user', 
     keys: %w[~/.ssh/BowWow.pem],
     forward_agent: true,
     auth_methods: %w[publickey]
