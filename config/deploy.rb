06e006600caedff647806b23a75a39d1cdaf9c4d
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-25 17:24:15 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ namespace :deploy do
   task :upload do
     on roles(:app) do |_host|
       execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
-      # upload!('config/master.key', "#{shared_path}/config/master.key")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
