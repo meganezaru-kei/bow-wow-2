fae4ea830b21449929a3aa260b41a3b61d60af92
Kei Nishiguchi <lostman0609@gmail.com>
2020-02-25 15:17:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-keys: ['~/.ssh/BowWow.pem'] 
+                  keys: ['~/.ssh/BowWow.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -26,7 +26,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # master.key用のシンボリックリンクを追加
-# set :linked_files, fetch(:linked_files, []).push('config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
