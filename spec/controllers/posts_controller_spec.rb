2e683996f731be4b26948e0f68a79ef187a897db
Kei Nishiguchi <lostman0609@gmail.com>
2020-03-01 22:08:23 +0900

    test posts controller
    postsコントローラのテスト

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 
 describe PostsController do
+  describe 'GET #index' do
+    it '投稿が新着順に表示されること' do
+      posts = create_list(:post, 3, :with_images)
+      get :index
+      expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at } )
+    end
+
+    it 'indexテンプレートをレンダリングすること' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    it 'リクエストされたpostのパラメータが@postに割り当てられていること' do
+      post = create(:post, :with_images)
+      get :show, params: { id: post }
+      expect(assigns(:post)).to eq post
+    end
+    
+    it 'showテンプレートをレンダリングすること' do
+      post = create(:post, :with_images)
+      get :show, params: { id: post }
+      expect(response).to render_template :show
+    end
+  end
+
   describe 'GET #new' do
     it 'newテンプレートをレンダリングすること' do
       get :new
@@ -10,28 +37,15 @@ describe PostsController do
 
   describe 'GET #edit' do
     it 'リクエストされたpostのパラメータが@postに割り当てられていること' do
-      # post = create(:post)
-      # get :edit, params: { id: post }
-      # expect(assigns(:post)).to eq post
+      post = create(:post, :with_images)
+      get :edit, params: { id: post }
+      expect(assigns(:post)).to eq post
     end
 
     it 'editテンプレートをレンダリングすること' do
-      # post = create(:post)
-      # get :edit, params: { id: post }
-      # expect(response).to render_template :edit
-    end
-  end
-
-  describe 'GET #index' do
-    it '投稿が新着順に表示されること' do
-      # posts = create_list(:post, 3)
-      # get :index
-      # expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at } )
-    end
-
-    it 'indexテンプレートをレンダリングすること' do
-      get :index
-      expect(response).to render_template :index
+      post = create(:post, :with_images)
+      get :edit, params: { id: post }
+      expect(response).to render_template :edit
     end
   end
 end
